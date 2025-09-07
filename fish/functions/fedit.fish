21513d9b0440476f369bda2a9879bb3ddcf3a84e
SantiagoCumpa <santiago.cumpa.dev@gmail.com>
2025-09-06 22:26:43 -0500

    add yazi

--- fish/functions/fedit.fish
+++ fish/functions/fedit.fish
@@ -1,4 +1,4 @@
 function fedit --description "Pick file → open in micro"
-    set -l file (fd --type f --hidden --exclude '*.vdi' | fzf --preview='bat --color=always --style=numbers --line-range :300 {}')
+    set -l file (fd --type f -H -E '*.vdi' | fzf --preview='bat --color=always --style=numbers --line-range :300 {}')
     test -n "$file"; and micro $file
 end
--- fish/functions/supac.fish
+++
@@ -1,4 +0,0 @@
-function supac --wraps='sudo pacman' --description 'alias supac=sudo pacman'
-  sudo pacman $argv
-        
-end
